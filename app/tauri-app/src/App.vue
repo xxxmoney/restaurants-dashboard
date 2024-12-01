45f76fa9da869dbd55fe30c2f745d6c35bc928a7
xxxmoney <xxxmoney111@gmail.com>
2024-12-01 17:47:08 +0100

    Small changes to styles

--- app/tauri-app/src/App.vue
+++ app/tauri-app/src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-
 </script>
 
 <template>
   <main>
-    <h1 class="text-xl text-center">Welcome to Tauri + Vue</h1>
-
-    <span class="text-xs text-green-600">TEXT</span>
-
+    <h1>TEST</h1>
   </main>
 </template>
 
