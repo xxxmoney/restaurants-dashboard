df1117b021079b72564ade42f7e7ed31fd993649
xxxmoney <xxxmoney111@gmail.com>
2024-12-02 03:22:10 +0100

    Fix

--- app/tauri-app/vite.config.js
+++ app/tauri-app/vite.config.js
@@ -8,7 +8,7 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
     plugins: [vue()],
-    base: '/restaurants-dashboard/',
+    base: '/',
 
     resolve: {
         alias: {
