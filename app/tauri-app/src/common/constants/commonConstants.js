fe34305156a602d8c95dcf9a488dc57b281c12b1
jakub.hana <jakub.hana@newtonmedia.cz>
2024-12-12 12:54:38 +0100

    Debug support missing stuff

--- app/tauri-app/src/common/constants/commonConstants.js
+++ app/tauri-app/src/common/constants/commonConstants.js
@@ -1,6 +1,6 @@
 
 const IS_DEBUG = true;
 
-export const PROXY_URL = IS_DEBUG ? '' : 'https://proxy-worker.xxxmoney111.workers.dev';
+export const PROXY_URL = IS_DEBUG ? 'http://localhost:8787' : 'https://proxy-worker.xxxmoney111.workers.dev';
 
 export const BACKEND_URL = IS_DEBUG ? 'http://localhost:8787' : 'https://restaurants-backend.xxxmoney111.workers.dev';
