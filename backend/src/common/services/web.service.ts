c2113f43a43919165dc53109d9a8d84a14c0373b
xxxmoney <xxxmoney111@gmail.com>
2024-12-18 20:26:06 +0100

    Web handling improvement

--- backend/src/common/services/web.service.ts
+++ backend/src/common/services/web.service.ts
@@ -1,48 +1,31 @@
 import {restaurantEnum} from "../../../../shared/enums/restaurant.enum";
 import {getHtmlDocFromUrl} from "../helpers/domParser.helper";
 import {RESTAURANTS} from "../../../../shared/constants/restaurant.constants";
+import {CheerioAPI} from "cheerio";
 
 export const WebService = {
-    getWebHtmlHandler(enumValue: Number) {
+    async getWebHtml(enumValue: Number) {
+        // @ts-ignore
+        const $ = await getHtmlDocFromUrl(RESTAURANTS[enumValue].url);
+
+        // Additional special handling
         switch (enumValue) {
-            case restaurantEnum.U_SISKU:
-                return async () => {
-                    // @ts-ignore
-                    const $ = await getHtmlDocFromUrl(RESTAURANTS[restaurantEnum.U_SISKU].url);
-                    return $.html();
-                }
-            case restaurantEnum.KLIKA:
-                return async () => {
-                    // @ts-ignore
-                    const $ = await getHtmlDocFromUrl(RESTAURANTS[restaurantEnum.KLIKA].url);
-                    return $.html();
-                }
             case restaurantEnum.BAR_RED_HOOK:
-                return async () => {
-                    // @ts-ignore
-                    const $ = await getHtmlDocFromUrl(RESTAURANTS[restaurantEnum.BAR_RED_HOOK].url);
+                // @ts-ignore
+                const innerFrame = $('iframe').first();
+                const innerFrameSrc = innerFrame.attr('src');
 
-                    const innerFrame = $('iframe').first();
-                    const innerFrameSrc = innerFrame.attr('src');
-
-                    if (innerFrameSrc) {
-                        const $inner = await getHtmlDocFromUrl(innerFrameSrc, 'windows-1250');
-                        innerFrame.attr('srcdoc', $inner.html());
-                    }
-
-                    // Set background color so its visible
-                    $('body').css('background-color', 'white');
-
-                    return $.html();
+                if (innerFrameSrc) {
+                    const $inner = await getHtmlDocFromUrl(innerFrameSrc, 'windows-1250');
+                    innerFrame.attr('srcdoc', $inner.html());
                 }
-            case restaurantEnum.PALATINO:
-                return async () => {
-                    // @ts-ignore
-                    const $ = await getHtmlDocFromUrl(RESTAURANTS[restaurantEnum.PALATINO].url);
-                    return $.html();
-                }
-            default:
-                throw new Error(`Restaurant with key ${enumValue} is not implemented`);
+
+                // Set background color so its visible
+                $('body').css('background-color', 'white');
+
+                break;
         }
+
+        return $.html();
     }
 }
