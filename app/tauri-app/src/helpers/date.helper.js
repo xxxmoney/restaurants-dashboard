b5c66b21d916cfda4310370514328d090a790580
xxxmoney <xxxmoney111@gmail.com>
2024-12-18 23:27:16 +0100

    Cosmetic improvements, current day

--- app/tauri-app/src/helpers/date.helper.js
+++ app/tauri-app/src/helpers/date.helper.js
@@ -1,6 +1,10 @@
 import {DateTime} from "luxon";
 import {DATE_FORMAT} from "root/shared/constants/common.constants.js";
 
-export const formatDate = (date) => {
-    return DateTime.fromFormat(date, DATE_FORMAT).toLocaleString(DateTime.DATE_MED)
+export const parseDate = (stringValue) => {
+    return DateTime.fromFormat(stringValue, DATE_FORMAT);
+}
+
+export const formatDate = (stringValue) => {
+    return parseDate(stringValue).toLocaleString(DateTime.DATE_MED)
 }
