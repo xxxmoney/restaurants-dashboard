858ea94b6613ee000a40fc2c558e1ef3a4814d4d
jakub.hana <jakub.hana@newtonmedia.cz>
2024-12-20 02:47:01 +0100

    Kv storage WIP

--- backend/src/routes/menu.route.ts
+++ backend/src/routes/menu.route.ts
@@ -5,10 +5,22 @@ const menuRoute = new Hono()
 
 // Get menu by id
 menuRoute.get('/:id', async (c) => {
+    // @ts-ignore
+    const kv = c.env.kvCache;
+    const cacheKey = 'menu-get-id-' + c.req.param('id');
+
+    const cachedValue = await kv.get(cacheKey);
+    console.log('Cached Value: ', cachedValue);
+    if (cachedValue) {
+        return c.json(JSON.parse(cachedValue));
+    }
+
     const id: number = parseInt(c.req.param('id'));
     
     // @ts-ignore
     const menus = await MenuService.getMenu(id, c.env.PROXY);
+    console.log('Caching value: ', menus);
+    await kv.put(cacheKey, JSON.stringify(menus), {expirationTtl: 10});
 
     return c.json(menus);
 });
