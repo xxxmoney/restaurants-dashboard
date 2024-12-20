65f8575d888709b485582d36fa3f835466e86477
jakub.hana <jakub.hana@newtonmedia.cz>
2024-12-20 02:52:42 +0100

    Correct proxy debug port

--- backend/src/common/constants/common.constants.ts
+++ backend/src/common/constants/common.constants.ts
@@ -1,4 +1,4 @@
 
-const IS_DEBUG = false;
+const IS_DEBUG = true;
 
-export const PROXY_URL = IS_DEBUG ? 'http://localhost:8787' : 'https://proxy-worker.xxxmoney111.workers.dev';
+export const PROXY_URL = IS_DEBUG ? 'http://localhost:8788' : 'https://proxy-worker.xxxmoney111.workers.dev';
