07887156f2db56240bd56c94bec3eadf315e28a6
xxxmoney <xxxmoney111@gmail.com>
2024-12-25 14:27:42 +0100

    Moved service test

--- backend/test/common/services/gemini.service.test.ts
+++ backend/test/common/services/gemini.service.test.ts
@@ -3,7 +3,8 @@ import {
 } from "cloudflare:test";
 import {describe, it, expect} from "vitest";
 
-import {GeminiService} from "../src/common/services/gemini.service";
+import {GeminiService} from "../../../src/common/services/gemini.service";
+import {Context} from "hono";
 
 describe("prmpt", () => {
     it("should respond with empty array", async () => {
@@ -16,12 +17,13 @@ describe("prmpt", () => {
         // Act
         //
 
-        // @ts-ignore
-        const result = await GeminiService.prompt({env}, "test");
+        const result = await GeminiService.prompt({env} as Context, "test");
 
+        
         //
         // Assert
         //
+
         expect(result).toStrictEqual([]);
 
     });
