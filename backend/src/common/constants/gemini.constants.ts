cfcf8033a3d861b9ec30ede061d723643dcd0cd3
xxxmoney <xxxmoney111@gmail.com>
2024-12-25 20:01:15 +0100

    Image prompt

--- backend/src/common/constants/gemini.constants.ts
+++ backend/src/common/constants/gemini.constants.ts
@@ -1,1 +1,3 @@
 export const MODEL_NAME = 'gemini-1.5-flash';
+
+export const DEFAULT_MIME_TYPE = 'image/jpeg';
--- backend/src/common/services/gemini.service.ts
+++ backend/src/common/services/gemini.service.ts
@@ -1,15 +1,33 @@
-import {GenerateContentResult, GenerativeModel, GoogleGenerativeAI} from "@google/generative-ai";
-import {MODEL_NAME} from "../constants/gemini.constants";
+import {GenerativeModel, GoogleGenerativeAI} from "@google/generative-ai";
+import {DEFAULT_MIME_TYPE, MODEL_NAME} from "../constants/gemini.constants";
+
+export interface Image {
+    base64: string,
+    mimeType?: string,
+}
 
 export class GeminiService {
-    private readonly model: GenerativeModel | undefined;
+    private readonly model: GenerativeModel;
 
     constructor(apiKey: string) {
         const generativeAI = new GoogleGenerativeAI(apiKey);
         this.model = generativeAI.getGenerativeModel({model: MODEL_NAME});
     }
 
-    public async prompt(prompt: string): Promise<GenerateContentResult> {
-        return await this.model!.generateContent([prompt]);
+    public async text(prompt: string): Promise<string> {
+        const content = await this.model.generateContent([prompt]);
+        return content.response.text();
+    }
+
+    public async image(prompt: string, image: Image): Promise<string> {
+        const imageData = {
+            inlineData: {
+                data: image.base64,
+                mimeType: image.mimeType ?? DEFAULT_MIME_TYPE,
+            }
+        }
+
+        const content = await this.model.generateContent([prompt, imageData]);
+        return content.response.text();
     }
 }
