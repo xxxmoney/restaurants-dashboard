d6cbeb8796f468af46c1410c84dcb32fd41e3474
xxxmoney <xxxmoney111@gmail.com>
2024-12-26 10:03:16 +0100

    Improvements to gemini service and tests

--- backend/src/common/services/gemini.service.ts
+++ backend/src/common/services/gemini.service.ts
@@ -1,10 +1,39 @@
-import {GenerativeModel, GoogleGenerativeAI} from "@google/generative-ai";
+import {GenerateContentResult, GenerativeModel, GoogleGenerativeAI} from "@google/generative-ai";
 import {DEFAULT_MIME_TYPE, MODEL_NAME} from "../constants/gemini.constants";
 
-export interface Image {
-    base64: string,
-    mimeType?: string,
+interface Image {
+    base64: string;
+    mimeType?: string;
 }
+
+class GeminiResponse {
+    public readonly content: GenerateContentResult;
+
+    constructor(content: GenerateContentResult) {
+        this.content = content;
+    }
+}
+
+export class TextReponse extends GeminiResponse {
+    constructor(content: GenerateContentResult) {
+        super(content);
+    }
+
+    public get text(): string {
+        return this.content.response.text().trim();
+    }
+}
+
+export class JsonResponse extends TextReponse {
+    constructor(response: TextReponse) {
+        super(response.content);
+    }
+
+    public get json(): any {
+        return JSON.parse(this.text);
+    }
+}
+
 
 export class GeminiService {
     private readonly model: GenerativeModel;
@@ -14,12 +43,16 @@ export class GeminiService {
         this.model = generativeAI.getGenerativeModel({model: MODEL_NAME});
     }
 
-    public async text(prompt: string): Promise<string> {
+    public async text(prompt: string): Promise<TextReponse> {
         const content = await this.model.generateContent([prompt]);
-        return content.response.text().trim();
+        return new TextReponse(content);
     }
 
-    public async image(prompt: string, image: Image): Promise<string> {
+    public async textJson(prompt: string): Promise<JsonResponse> {
+        return new JsonResponse(await this.text(prompt));
+    }
+
+    public async image(prompt: string, image: Image): Promise<TextReponse> {
         const imageData = {
             inlineData: {
                 data: image.base64,
@@ -28,6 +61,11 @@ export class GeminiService {
         }
 
         const content = await this.model.generateContent([prompt, imageData]);
-        return content.response.text().trim();
+        return new TextReponse(content);
     }
+
+    public async imageJson(prompt: string, image: Image): Promise<JsonResponse> {
+        return new JsonResponse(await this.image(prompt, image));
+    }
+
 }
