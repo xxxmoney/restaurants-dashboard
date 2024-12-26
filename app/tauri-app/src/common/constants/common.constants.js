cf593428357ae7b4bcd6166a48ad042136f982f0
xxxmoney <xxxmoney111@gmail.com>
2024-12-26 17:15:10 +0100

    Move is debug to shared

--- app/tauri-app/src/common/constants/common.constants.js
+++ app/tauri-app/src/common/constants/common.constants.js
@@ -1,3 +1,3 @@
-const IS_DEBUG = false;
+import {IS_DEBUG} from "root/shared/constants/common.constants.js";
 
 export const BACKEND_URL = IS_DEBUG ? 'http://localhost:8787' : 'https://restaurants-backend.xxxmoney111.workers.dev';
