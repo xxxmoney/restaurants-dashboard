ea308787bfb6baeb3d67ec62a7d20a2b46a08539
xxxmoney <xxxmoney111@gmail.com>
2024-12-30 11:12:20 +0100

    U sisku prompt improvement

--- backend/src/common/constants/gemini.constants.ts
+++ backend/src/common/constants/gemini.constants.ts
@@ -7,5 +7,5 @@ export const DEFAULT_MIME_TYPE = 'image/jpeg';
 export const COMPLY_WITH_SCHEMA_PROMPT = '{0}; Satisfy the previous prompt while returning only JSON that complies with the following YUP schema strictly: {1}';
 
 export const MENU_PROMPTS = {
-    [restaurantEnum.U_SISKU]: 'Using the provided screenshot of menu from the restaurant U Sisku, return the menus for each day, if there are none, return empty, stick strictly to what is only visible in the screenshot',
+    [restaurantEnum.U_SISKU]: 'Using the provided screenshot of menu from the restaurant U Sisku, return the menus for each day, if there are none, return empty, stick strictly to what is only visible in the screenshot - if there are no typical menus visible, return empty',
 }
