10e176c2a682907733027e5514f290e3bce45517
jakub.hana <jakub.hana@newtonmedia.cz>
2025-01-24 08:14:31 +0100

    Improve prompts

--- backend/src/common/constants/gemini.constants.ts
+++ backend/src/common/constants/gemini.constants.ts
@@ -5,8 +5,8 @@ export const MODEL_NAME = 'gemini-1.5-flash-8b';
 
 export const DEFAULT_MIME_TYPE = 'image/jpeg';
 
-export const COMPLY_WITH_SCHEMA_PROMPT = '{0}; Satisfy the previous prompt while returning only JSON that complies with the following YUP schema strictly: {1}';
+export const COMPLY_WITH_SCHEMA_PROMPT = '{0}; Satisfy the previous prompt while returning only JSON that complies with the following YUP schema strictly, make sure to comply with formats for each field: {1}';
 
 export const MENU_PROMPTS = {
-    [restaurantEnum.CINKY_LINKY]: `Using the provided screenshot of menu from the restaurant U Sisku, make sure that the year of the dates of menus is ${DateTime.now().year}!, return the menus for each day, if there are none, return empty, stick strictly to what is only visible in the screenshot`,
+    [restaurantEnum.CINKY_LINKY]: `You are a restaurant menu digitizer. Convert the provided restaurant menu screenshot into a JSON representation, organized by day.`,
 }
