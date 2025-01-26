5d8aee1d8611ad62a39c12a19b3c6809257f6f63
xxxmoney <xxxmoney111@gmail.com>
2025-01-26 07:32:44 +0100

    Unused file

--- app/tauri-app/src/common/constants/commonConstants.js
+++
@@ -1,6 +0,0 @@
-
-const IS_DEBUG = false;
-
-export const PROXY_URL = IS_DEBUG ? 'http://localhost:8787' : 'https://proxy-worker.xxxmoney111.workers.dev';
-
-export const BACKEND_URL = IS_DEBUG ? 'http://localhost:8787' : 'https://restaurants-backend.xxxmoney111.workers.dev';
