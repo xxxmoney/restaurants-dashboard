af1f87061a62afbc723ff66efa9bd179e4716f54
xxxmoney <xxxmoney111@gmail.com>
2025-02-22 19:55:27 +0100

    Update model

--- backend/src/common/constants/gemini.constants.ts
+++ backend/src/common/constants/gemini.constants.ts
@@ -1,6 +1,6 @@
 import {restaurantEnum} from "../../../../shared/enums/restaurant.enum";
 
-export const MODEL_NAME = 'gemini-1.5-flash-8b';
+export const MODEL_NAME = 'gemini-2.0-flash-lite-preview-02-05';
 
 export const DEFAULT_MIME_TYPE = 'image/jpeg';
 
