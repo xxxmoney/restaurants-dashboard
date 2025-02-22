81a07ed2312312dac34b86782f2477927706e240
xxxmoney <xxxmoney111@gmail.com>
2025-02-22 19:58:43 +0100

    Add test cases

--- backend/test/common/services/menu.service.test.ts
+++ backend/test/common/services/menu.service.test.ts
@@ -5,7 +5,16 @@ import {restaurantEnum} from "../../../../shared/enums/restaurant.enum";
 
 
 describe("getMenu", () => {
-    it("should get menu", async () => {
+    it("should get menu cinky linky", async () => {
+        const restaurantId = restaurantEnum.CINKY_LINKY;
+
+        const result = await MenuService.getMenu(restaurantId, env);
+
+        expect(result).not.null;
+        console.log(JSON.stringify(result, null, 2));
+    });
+
+    it("should get menu klika", async () => {
         const restaurantId = restaurantEnum.KLIKA;
 
         const result = await MenuService.getMenu(restaurantId, env);
@@ -13,4 +22,24 @@ describe("getMenu", () => {
         expect(result).not.null;
         console.log(JSON.stringify(result, null, 2));
     });
+
+    it("should get menu red hook", async () => {
+        const restaurantId = restaurantEnum.BAR_RED_HOOK;
+
+        const result = await MenuService.getMenu(restaurantId, env);
+
+        expect(result).not.null;
+        console.log(JSON.stringify(result, null, 2));
+    });
+
+    it("should get menu palatino", async () => {
+        const restaurantId = restaurantEnum.PALATINO;
+
+        const result = await MenuService.getMenu(restaurantId, env);
+
+        expect(result).not.null;
+        console.log(JSON.stringify(result, null, 2));
+    });
+
+
 });
