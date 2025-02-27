3a2a5da6ee7f77dd9088978430e6c2351bcf4609
xxxmoney <xxxmoney111@gmail.com>
2025-02-27 10:24:57 +0100

    Remove space

--- backend/src/common/constants/common.constants.ts
+++ backend/src/common/constants/common.constants.ts
@@ -4,4 +4,3 @@ export const FORCE_USE_REMOTE_PROXY = false;
 
 export const REMOTE_PROXY_URL = 'https://proxy-worker.xxxmoney111.workers.dev';
 export const PROXY_URL = IS_DEBUG ? 'http://localhost:8788' : REMOTE_PROXY_URL;
-
