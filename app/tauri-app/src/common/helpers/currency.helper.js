49a10faa0b07ede335c924949e6459bdebea0fa1
xxxmoney <xxxmoney111@gmail.com>
2025-03-10 19:41:33 +0100

    Handle missing price on FE

--- app/tauri-app/src/common/helpers/currency.helper.js
+++ app/tauri-app/src/common/helpers/currency.helper.js
@@ -1,6 +1,8 @@
+import {CURRENCY} from "@/common/constants/common.constants.js";
+
 export const formatCurrency = (value) => {
     return value.toLocaleString('cs-CZ', {
         style: 'currency',
-        currency: 'CZK'
+        currency: CURRENCY
     });
 };
