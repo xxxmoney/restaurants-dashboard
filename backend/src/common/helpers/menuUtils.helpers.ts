1bfb9ddd721f24c49207d896434cb374058327f7
Jakub Hána <61245332+xxxmoney@users.noreply.github.com>
2025-04-01 10:23:39 +0200

    Merge fix-menus to master (#53)
    * Fix cinky linky
    
    * Temp fix vozovna pankrac
    
    * Fix parse date regex
    
    * Move regex to menu constants
    
    ---------
    
    Co-authored-by: jakub.hana <jakub.hana@newtonmedia.cz>

--- backend/src/common/helpers/menuUtils.helpers.ts
+++ backend/src/common/helpers/menuUtils.helpers.ts
@@ -1,8 +1,20 @@
 import {DateTime} from "luxon";
+import {DATE_REGEX} from "../constants/menu.constants";
 
 export function parseDate(text: string) {
-    const date = text.match(/\d{1,2}\.\d{1,2}\.\d{4}/g)![0];
-    return DateTime.fromFormat(date, 'd.M.yyyy');
+    const dateMatch = text.match(DATE_REGEX);
+
+    // Check if matched
+    if (!dateMatch) {
+        throw new Error('Date not matched');
+    }
+
+    // Get groups from match
+    const day = parseInt(dateMatch.groups!.day);
+    const month = parseInt(dateMatch.groups!.month);
+    const year = parseInt(dateMatch.groups!.year);
+
+    return DateTime.fromObject({day, month, year});
 }
 
 export function parsePrice(text: string) {
