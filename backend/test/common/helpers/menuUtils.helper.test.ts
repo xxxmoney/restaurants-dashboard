b5bba60907b7540d884bff535cf873ad6f28e5dc
jakub.hana <jakub.hana@newtonmedia.cz>
2025-04-02 11:38:52 +0200

    Remove unused it

--- backend/test/common/helpers/menuUtils.helper.test.ts
+++ backend/test/common/helpers/menuUtils.helper.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, it, test} from "vitest";
+import {describe, expect, test} from "vitest";
 import {DateTime} from "luxon";
 import {parseDate} from "../../../src/common/helpers/menuUtils.helper";
 
