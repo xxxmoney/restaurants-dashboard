18b3706b11a31287015dc8fe4b427fce75410f2a
jakub.hana <jakub.hana@newtonmedia.cz>
2025-04-02 11:38:40 +0200

    Better error message, test for menu utils date parse

---
+++ backend/test/common/helpers/menuUtils.helper.test.ts
@@ -0,0 +1,17 @@
+import {describe, expect, it, test} from "vitest";
+import {DateTime} from "luxon";
+import {parseDate} from "../../../src/common/helpers/menuUtils.helper";
+
+describe("parseDate", () => {
+    const cases: [string, DateTime][] = [
+      ['Polední Menu 2.4. 2025', DateTime.fromObject({day: 2, month: 4, year: 2025})],
+      ['2.4.2025', DateTime.fromObject({day: 2, month: 4, year: 2025})],
+      ['2.4.', DateTime.fromObject({day: 2, month: 4, year: 2025})],
+    ];
+
+    test.each(cases)("parseDate(%p) should return %p", (input, expected) => {
+        const result = parseDate(input);
+
+        expect(result).toEqual(expected);
+    });
+});
