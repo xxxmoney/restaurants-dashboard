2c21431b0383f3f44bf6a0cca6333678ff2cc67e
jakub.hana <jakub.hana@newtonmedia.cz>
2025-04-04 08:48:04 +0200

    Handler for scheduled tasks

--- backend/src/scheduled/index.ts
+++ backend/src/scheduled/index.ts
@@ -2,22 +2,30 @@ import {DateTime} from "luxon";
 import {REFRESH_CACHE_CRON} from "../common/constants/scheduled.constants";
 import {handleRefreshCache} from "./refreshCache.scheduled";
 
+async function handler(event: ScheduledEvent, func: Promise<void>): Promise<void> {
+    try {
+        await func;
+        console.info(`Scheduled task completed successfully: '${event.cron}'`);
+    } catch (e: any) {
+        // Throw error with the message
+        throw new Error(`Scheduled task failed: '${event.cron}': ${e.message}`);
+    }
+}
+
 export async function scheduled(event: ScheduledEvent, env: any, ctx: ExecutionContext): Promise<void> {
     console.info(`Cron Trigger: ${event.cron}, Scheduled Time: ${DateTime.fromMillis(event.scheduledTime)}`);
 
-    let scheduledTaskPromise: Promise<void>;
+    let handlerPromise: Promise<void>;
 
     // Handle jobs based on cron
     switch (event.cron) {
         case REFRESH_CACHE_CRON:
-            scheduledTaskPromise = handleRefreshCache(env);
+            handlerPromise = handler(event, handleRefreshCache(env));
             break;
         default:
             throw new Error(`Unhandled cron schedule: ${event.cron}`);
     }
 
     // Use ctx.waitUntil to ensure async task completes
-    if (scheduledTaskPromise) {
-        ctx.waitUntil(scheduledTaskPromise);
-    }
+    ctx.waitUntil(handlerPromise);
 }
