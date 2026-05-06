bd1dca5a7ab5db099d219a9346a11ac8c0353bef
Jakub Hána <61245332+xxxmoney@users.noreply.github.com>
2026-05-06 10:16:24 +0200

    Merge fix-llm-version to master

--- backend/src/common/constants/gemini.constants.ts
+++ backend/src/common/constants/gemini.constants.ts
@@ -2,7 +2,7 @@ import {restaurantEnum} from "../../../../shared/enums/restaurant.enum";
 
 export const LANGUAGE = 'CZECH';
 
-export const MODEL_NAME = 'gemma-3-27b-it';
+export const MODEL_NAME = 'gemini-3.1-flash-lite-preview';
 
 export const DEFAULT_MIME_TYPE = 'image/jpeg';
 
