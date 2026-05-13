d3469fa3bb60bde4dbaa3f7a4a7ee93480dfb7d3
jakub.hana <jakub.hana@sprinx.com>
2026-05-13 09:06:21 +0200

    Allowed restaurants, limit thus refreshing cache, etc

--- app/tauri-app/src/common/constants/menu.constants.js
+++ app/tauri-app/src/common/constants/menu.constants.js
@@ -1,23 +1,9 @@
-import {restaurantEnum} from "root/shared/enums/restaurant.enum.js";
+import {allowedRestaurants} from "root/shared/enums/restaurant.enum.js";
 import {RESTAURANTS} from "root/shared/constants/restaurant.constants.js";
 
-export const MENUS = [
-    {
-        id: restaurantEnum.NOVODVORKA,
-        name: RESTAURANTS[restaurantEnum.NOVODVORKA].name,
-    },
-    {
-        id: restaurantEnum.MINI_GOLF,
-        name: RESTAURANTS[restaurantEnum.MINI_GOLF].name,
-    },
-    {
-        id: restaurantEnum.IL_GIARDINO,
-        name: RESTAURANTS[restaurantEnum.IL_GIARDINO].name,
-    },
-    {
-        id: restaurantEnum.KAMYK,
-        name: RESTAURANTS[restaurantEnum.KAMYK].name,
-    }
-];
+export const MENUS = allowedRestaurants.map(restaurantId => ({
+    id: restaurantId,
+    name: RESTAURANTS[restaurantId].name,
+}));
 
 export const MENUS_PER_ROW = 4;
