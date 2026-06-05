a3d28b8e36c8a3ba4166f1d5ea021b1a60f38613
jakub.hana <jakub.hana@sprinx.com>
2026-06-05 13:02:59 +0200

    Fixed issues with dependencies for better-auth - ABSOLUTE CINEMA

--- backend/src/common/composables/auth.comp.ts
+++ backend/src/common/composables/auth.comp.ts
@@ -1,9 +1,13 @@
+
 import { betterAuth } from "better-auth";
+import { dash } from "@better-auth/infra";
 
 export const useAuth = (env: any) => {
   const auth = betterAuth({
     // TODO: setup auth
-
+    plugins: [
+      dash()
+    ],
     socialProviders: {
       google: {
         clientId: env.GOOGLE_CLIENT_ID!,
