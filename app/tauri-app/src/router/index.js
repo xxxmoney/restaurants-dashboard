17a665be524f243eec8e894a9c7a4545b8deb786
jakub.hana <jakub.hana@sprinx.com>
2026-06-10 08:46:09 +0200

    Fix route now functional with changeable paths, WIP route authentication

--- app/tauri-app/src/router/index.js
+++ app/tauri-app/src/router/index.js
@@ -1,20 +1,36 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
 import WebsitesView from "@/views/WebsitesView.vue";
 import MenusView from "@/views/MenusView.vue";
 
-const routes = [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/websites', name: 'websites', component: WebsitesView },
-    { path: '/menus', name: 'menus', component: MenusView },
-];
-
 const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
+    history: createWebHistory('/restaurants-dashboard/'),
+    routes: [
+      { path: '/', name: 'home', component: HomeView },
+      { path: '/websites', name: 'websites', component: WebsitesView },
+      { path: '/menus', name: 'menus', component: MenusView },
+    ]
 });
 
 // TODO: maybe use authentication verification here with beforeEach?
+// router.beforeEach(async (to, from, next) => {
+//   const authStore = useAuthStore();
+//
+//   // 🚨 Crucial: Wait for Better Auth to check the session BEFORE doing anything else
+//   await authStore.initializeAuth();
+//
+//   // Now the store is 100% guaranteed to be populated
+//   if (to.meta.requiresAuth && !authStore.user) {
+//     // If route requires auth and user isn't logged in, send to login
+//     next('/login');
+//   } else if (to.path === '/login' && authStore.user) {
+//     // If logged in user tries to go to login page, send to dashboard
+//     next('/dashboard');
+//   } else {
+//     // Otherwise, carry on!
+//     next();
+//   }
+// });
 
 export { router }
