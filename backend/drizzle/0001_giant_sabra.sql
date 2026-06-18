d53fff18161696358898432098dffbad94e67fd9
jakub.hana <jakub.hana@sprinx.com>
2026-06-18 13:12:09 +0200

    Update favorites schema, migration

---
+++ backend/drizzle/0001_giant_sabra.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `favorites` ADD `restaurant_id` text NOT NULL;
--- backend/src/db/favorites.schema.ts
+++ backend/src/db/favorites.schema.ts
@@ -1,12 +1,14 @@
 import { relations } from "drizzle-orm";
 import { sqliteTable, text, integer, index } from "drizzle-orm/sqlite-core";
 import { users } from "./auth.schema";
+import {allowedRestaurants} from "../../../shared/enums/restaurant.enum";
 
 export const favorites = sqliteTable(
   "favorites",
   {
     id: text("id").primaryKey().$defaultFn(() => crypto.randomUUID()),
     date: integer("date", { mode: "timestamp" }).notNull(),
+    restaurantId: text("restaurant_id", { enum: allowedRestaurants.map(v => v.toString()) as [string, ...string[]] }).notNull(),
     text: text("text").notNull(),
     userId: text("user_id")
       .notNull()
